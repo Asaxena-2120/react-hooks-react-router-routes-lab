c23ad06d5be5335ea9b4c470b5274518aeca5e71
Aastha <saxena.aastha91@gmail.com>
2023-01-02 16:39:02 -0500

    commit tree e1f0debb30f1571cb1e20352944a8bdd6e529c15

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorsList = actors.map((actor)=>(
+    <div key={actor.name}> 
+    <h2>Name: {actor.name}</h2>
+    
+    <p>Movies:</p>
+    <ul>
+      {actor.movies.map((movie)=>(<li>{movie}</li>))}
+    </ul>
+  </div>
+  ))
+  return <div>
+    <h1>Actor Page</h1>
+    {actorsList}
+    </div>;
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+  const movieList = movies.map((movie)=>(
+    <div>
+      <h2>Name: {movie.title}</h2>
+      <p>Runtime: {movie.time}</p>
+      {/* <p>Genres:</p> */}
+      <ul>
+        {movie.genres.map((genre)=>(<li>{genre}</li>))}
+      </ul>
+    </div>
+  ))
+  return (<div>
+    <h1> Movies Page</h1>
+    {movieList}
+  </div>);
 }
 
 export default Movies;
